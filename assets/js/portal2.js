f30afcfa563c61c89b012fc90b6f193cbbd4bc9a
JessicaMelo03 <jmeloferreira3@gmail.com>
2017-07-25 15:42:48 -0300

    portal

---
+++ assets/js/portal2.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
+    var objs = document.getElementsByClassName("slide");
+    
+    
+    for (var i in objs)
+        {
+            objs[i].style.width = slidewidth;
+            
+        }
+}
+
